b982f832954ba4950ecfbc16b61bb864c7dc98c7
Mohanraj <mohanrajlenova@gmail.com>
2024-07-07 19:27:41 +0530

    registrations

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register, name="register"),
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Welcome to Django CRM")
+            return redirect('index')
+        else:
+            messages.error(request, "Bad Credentials")
+            return redirect('index')
+    context = {}
+    return render(request, "index.html", context=context)
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Your Successfully logged out")
+    return redirect('index')
+
+def register(request):
+    context = {}
+    return render(request, "register.html", context=context)
